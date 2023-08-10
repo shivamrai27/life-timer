b98654daca342b70347f55325fd555867f883ecc
Shivam Rai <200shivamrai@gmail.com>
2023-08-11 03:18:13 +0530

    Life Timer

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let dateOfBirth = undefined;
+let dateOfBirth;
 const initialTextEl = document.getElementById("initialText");
 const afterTextEl = document.getElementById("afterText");
-const buttonEl = document.getElementById('dobButton');
+const buttonEl = document.getElementById("dobButton");
 const dobInputEl = document.getElementById('dob');
 
 
-const setDOBHandler = function setDOBHandler() => {
+const setDOBHandler = () => {
     dateOfBirth = dobInputEl.value;
     console.log("DOB id ", dateOfBirth);
+    if (dateOfBirth) {
+        initialTextEl.classList.add("hide");
+        afterTextEl.classList.remove("hide");
+    } else {
+        afterTextEl.classList.add("hide");
+        initialTextEl.classList.remove("hide");
+    }
 };
 buttonEl.addEventListener("click", setDOBHandler);
